4de65b90ed8cfbc218fb65d2d4e7bff32aea2035
kurosame <kurosame1227@gmail.com>
2020-02-03 20:11:03 +0900

    fix: e2e

--- test/e2e/cypress/integration/test.spec.ts
+++ test/e2e/cypress/integration/test.spec.ts
@@ -8,27 +8,43 @@ context('Test', () => {
   })
 
   it('Initial display', () => {
-    cy.get('[data-test="count"]').should('have.text', '0')
-    cy.get('[data-test="saga-count"]').should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .first()
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .last()
+      .should('have.text', '0')
 
     cy.get('#root').screenshot('initial-display')
   })
 
   it('Click the add-count, update the count', () => {
-    cy.get('[data-test="add-count"]').click()
+    cy.get('[data-test="add-count"]')
+      .first()
+      .click()
 
-    cy.get('[data-test="count"]').should('have.text', '1')
-    cy.get('[data-test="saga-count"]').should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .first()
+      .should('have.text', '1')
+    cy.get('[data-test="count"]')
+      .last()
+      .should('have.text', '0')
 
     cy.get('#root').screenshot('add-count')
   })
 
-  it('Click the add-saga-count, update the sagaCount', () => {
-    cy.get('[data-test="add-saga-count"]').click()
+  it('Click the add-count for saga, update the count for saga', () => {
+    cy.get('[data-test="add-count"]')
+      .last()
+      .click()
 
-    cy.get('[data-test="count"]').should('have.text', '0')
-    cy.get('[data-test="saga-count"]').should('have.text', '2')
+    cy.get('[data-test="count"]')
+      .first()
+      .should('have.text', '0')
+    cy.get('[data-test="count"]')
+      .last()
+      .should('have.text', '2')
 
-    cy.get('#root').screenshot('add-saga-count')
+    cy.get('#root').screenshot('add-count-saga')
   })
 })
