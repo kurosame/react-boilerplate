5ce7d77c62bbbecf866f7954d5a358f8ed4cc5c4
kurosame <kurosame1227@gmail.com>
2020-03-25 17:54:39 +0900

    fix

--- src/containers/Parent.tsx
+++ src/containers/Parent.tsx
@@ -1,16 +1,17 @@
 import React, { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from 'redux'
 import Child from '@/components/Child'
-import { ADD_COUNT, GET_SAGA_COUNT } from '@/modules/counter'
+import { CounterState, ADD_COUNT, GET_SAGA_COUNT } from '@/modules/counter'
 import { States } from '@/modules/states'
 
 const Parent: React.FC = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch<Dispatch>()
   const addCount = useCallback(() => dispatch({ type: ADD_COUNT }), [dispatch])
   const getSagaCount = useCallback(() => dispatch({ type: GET_SAGA_COUNT }), [
     dispatch
   ])
-  const counter = useSelector((s: States) => s.counter)
+  const counter = useSelector<States, CounterState>(s => s.counter)
 
   return (
     <>
